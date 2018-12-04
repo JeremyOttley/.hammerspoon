ef8efc6798c5a140e14041c99877f09a754563c8
Jeremy Ottley <Gazaunga@gmail.com>
2018-12-03 19:42:10 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
+-- Load Lunette
+hs.loadSpoon("Lunette")
 
+-- Default Lunette configuration
+spoon.Lunette:bindHotkeys()
+
+--]] example custom map with `leftHalf` and `rightHalf` reconfigured and the history commands disabled
+
+--customBindings = {
+--  leftHalf = {{"Q", "W"}, "E"},
+--  rightHalf = {{"A", "S"}, "D"},
+--  undo = false,
+--  redo = false
+--}
+
+--spoon.Lunette:bindHotkeys(customBindings) [[--
