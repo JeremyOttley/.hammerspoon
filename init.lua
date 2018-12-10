d593d181762cc094ce4212df373ccb146dc51753
Jeremy Ottley <jeremy.ottley@gmail.com>
2018-12-10 07:53:38 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,32 +1,3 @@
--- Welcome Message
---local alert_sound = hs.sound.getByFile("alert.wav")
---alert_sound:play() 
-
---local weather = require("hs-weather")
---weather.start()
-
-pomo = require('utils/pomo')
-
--- App vars
---local browser   = hs.appfinder.appFromName("Google Chrome")
---local term      = hs.appfinder.appFromName("xterm-256color")
---local bbedit    = hs.appfinder.appFromName("BBEdit")
---local outlook   = hs.appfinder.appFromName("Outlook")
---local finder    = hs.appfinder.appFromName("Finder")
---local slack     = hs.appfinder.appFromName("Slack")
-
--- Set window animation off. It's much smoother.
-hs.window.animationDuration = 0
-
--- Remove window shadows
-hs.window.setShadows(true)
-
---hs.hotkey.bind(hyper, 'D', function() hs.alert.show(os.date("%A %b %d, %Y - %I:%M%p"), 4) end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
-end)
-
 -- Reload on config change
 function reloadConfig(files)
     doReload = false
@@ -40,17 +11,75 @@ function reloadConfig(files)
     end
 end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
---do I need this alert?
 hs.alert.show("Hammerspoon ready!", 3)
---hs.sound.getByFile("alert.wav"):play()
-
-
--- [[ Window Movement ]] --
 
 -- modkeys/chords
 modkey = {"cmd","alt"}
+
+-- {'⌘', '⌥', 'ctrl'}
 local hyper = {'ctrl', 'alt', 'cmd'}
+
+--{'⌘', '⌥', '⇧', 'ctrl'}
 local hyperShift = {'ctrl', 'alt', 'cmd', 'shift'}
+
+-- Modules
+local fnutils = hs.fnutils
+local each = fnutils.each
+local partial = fnutils.partial
+local concat = fnutils.concat
+local indexOf = fnutils.indexOf
+
+local eventtap = hs.eventtap
+local keyStroke = eventtap.keyStroke
+
+local app = hs.application
+local frontmost = app.frontmostApplication
+local watcher = app.watcher
+local get = app.get
+local launchOrFocus = app.launchOrFocus
+
+local screen = hs.screen
+local allScreens = screen.allScreens
+local mainScreen = screen.mainScreen
+local setPrimary = screen.setPrimary
+
+local grid = hs.grid
+
+-- Other local variables
+local shift = 0.075
+
+--
+-- [[ Functions ]] --
+--
+
+-- open
+function open(name)
+    return function()
+        hs.application.launchOrFocus(name)
+        if name == 'Finder' then
+            hs.appfinder.appFromName(name):activate()
+        end
+    end
+end
+
+-- open key folders with ctrl + key
+function directoryLaunchKeyRemap(mods, key, dir)
+    local mods = mods or {}
+    hs.hotkey.bind(mods, key, function()
+        local shell_command = "open " .. dir
+        hs.execute(shell_command)
+    end)
+end
+
+--
+-- [[ Windows ]] --
+--
+
+-- Set window animation off. It's much smoother.
+hs.window.animationDuration = 0
+
+-- Remove window shadows
+hs.window.setShadows(true)
 
 -- tile left
 hs.hotkey.bind(modkey, "Left", function()
@@ -80,34 +109,24 @@ hs.hotkey.bind(modkey, "Right", function()
   win:setFrame(f)
 end)
 
--- open
-function open(name)
-    return function()
-        hs.application.launchOrFocus(name)
-        if name == 'Finder' then
-            hs.appfinder.appFromName(name):activate()
-        end
-    end
-  end
-  
-  --- quick open applications
-  hs.hotkey.bind({"alt"}, "pad1", open("Finder"))
-  hs.hotkey.bind({"alt"}, "pad2", open("xterm-256color"))
-  hs.hotkey.bind({"alt"}, "pad3", open("BBEdit"))
-  hs.hotkey.bind({"alt"}, "pad4", open("iTunes"))
-  hs.hotkey.bind({"alt"}, "o", open("/Applications/Outlook"))
+--[[ Window Filter ]]--
+-- set up your windowfilter
+-- switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
+-- switcher_space = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
+-- switcher_browsers = hs.window.switcher.new{'Safari','Google Chrome'} -- specialized switcher for your dozens of browser windows :)
 
--- Determine all essential folders
--- Map them to letters instead of numbers for now
+-- bind to hotkeys; WARNING: at least one modifier key is required!
+-- hs.hotkey.bind('alt','tab','Next window',function()switcher:next()end)
+-- hs.hotkey.bind('alt-shift','tab','Prev window',function()switcher:previous()end)
 
--- open key folders with ctrl + 1,2,3,
-function directoryLaunchKeyRemap(mods, key, dir)
-    local mods = mods or {}
-    hs.hotkey.bind(mods, key, function()
-        local shell_command = "open " .. dir
-        hs.execute(shell_command)
-    end)
-end
+-- alternatively, call .nextWindow() or .previousWindow() directly (same as hs.window.switcher.new():next())
+-- hs.hotkey.bind('alt','tab','Next window',hs.window.switcher.nextWindow)
+-- you can also bind to `repeatFn` for faster traversing
+-- hs.hotkey.bind('alt-shift','tab','Prev window',hs.window.switcher.previousWindow,nil,hs.window.switcher.previousWindow)
+
+--
+-- [[ Commands ]] --
+--
 
 --- quick open folders
 directoryLaunchKeyRemap({"ctrl"}, "D", "~/Documents") 
@@ -145,4 +164,7 @@ hs.hotkey.bind({"alt", "shift"}, 'O', function()
 --hs.hotkey.bind({"alt"}, '<>', function()
 --  hs.application.launchOrFocus("Adobe Acrobat Pro")
 --      end)
+
+-- raise console
+hs.hotkey.bind({"ctrl", "cmd", "alt"}, "Y", hs.toggleConsole)
 
